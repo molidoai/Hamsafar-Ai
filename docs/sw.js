90118a450c1462e2e3145c18c86163b321918363
molidoai <molidoai@gmail.com>
2026-09-06 01:50:53 +0330

    Add trip journal and refresh offline hub cache list

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,6 @@
-const CACHE = "hamsafar-031";
-const FILES = ["./","./index.html","./styles.css","./map.html","./trip.html","./sos.html","./speed.html","./family.html","./plans.html","./security.html","./offline-vault.js","./more.html"];
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
-});
+const CACHE = "hamsafar-032";
+const FILES = ["./index.html","./styles.css","./map.html","./trip.html","./sos.html","./speed.html","./family.html","./plans.html","./security.html","./offline-vault.js","./more.html","./checklist.html","./journal.html"];
+self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES))); });
 self.addEventListener("fetch", (e) => {
   e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request).then((res) => {
     const copy = res.clone();
