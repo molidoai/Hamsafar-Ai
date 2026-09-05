f566a41851a3ac73b2d5ddd1560b1684358ce43a
molidoai <molidoai@gmail.com>
2026-09-06 01:54:10 +0330

    Add 12 Iranian seasons pricing chapter and seasonal travel notes

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,4 @@
-const CACHE="hamsafar-033";
-const FILES=["./index.html","./styles.css","./map.html","./trip.html","./sos.html","./speed.html","./family.html","./plans.html","./security.html","./offline-vault.js","./more.html","./checklist.html","./journal.html","./fuel.html","./numbers.html","./expense.html"];
+const CACHE="hamsafar-034";
+const FILES=["./index.html","./styles.css","./map.html","./trip.html","./sos.html","./speed.html","./family.html","./plans.html","./security.html","./offline-vault.js","./more.html","./checklist.html","./journal.html","./fuel.html","./numbers.html","./expense.html","./seasons.html","./season-guide.html"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;}).catch(()=>caches.match("./index.html"))));
-});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;}).catch(()=>caches.match("./index.html"))));});
