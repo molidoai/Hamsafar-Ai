ee4522031ce7fdba63aaea020eec963b250e4c57
molidoai <molidoai@gmail.com>
2026-09-06 08:55:05 +0330

    Propagate free full access across hub and service worker

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hamsafar-039";
-const FILES=["./index.html","./styles.css","./more.html","./android.html","./update.html","./help.html","./about.html","./backup.html","./security.html","./offline-vault.js","./trip.html","./map.html","./sos.html","./plans.html"];
+const CACHE="hamsafar-040";
+const FILES=["./index.html","./styles.css","./access.js","./more.html","./plans.html","./seasons.html","./android.html","./update.html","./help.html","./about.html","./backup.html","./security.html","./offline-vault.js","./trip.html","./map.html","./sos.html","./family.html"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;}).catch(()=>caches.match("./index.html"))));});
